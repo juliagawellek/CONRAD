c01de9a9fb8544525156e079c05947a288b69ef8
Julia Gawellek <ec49eliv@stud.informatik.uni-erlangen.de>
2016-04-26 11:54:28 +0200

    uebung 1 fertig

--- src/edu/stanford/rsl/tutorial/JuliaG/BIO.java
+++ src/edu/stanford/rsl/tutorial/JuliaG/BIO.java
@@ -2,17 +2,20 @@ package edu.stanford.rsl.tutorial.JuliaG;
 
 import ij.ImageJ;
 import edu.stanford.rsl.conrad.data.numeric.Grid2D;
+import edu.stanford.rsl.conrad.data.numeric.NumericGrid;
+import edu.stanford.rsl.conrad.data.numeric.NumericPointwiseOperators;
 
 public class BIO extends Grid2D {
 
 	public BIO (int x, int y){
 		super(x, y);
 		//this.square(70,70,50,5);
-		this.filledcircle(270, 270, 220, 2);
-		this.filledellipse(2,1, 160, 180, 20, 1);
-		this.filledellipse(2,1, 400, 180, 20, 1);
-		this.filledellipse(2,1, 280, 400, 40, 1);
+		this.filledcircle(250, 250, 200, 2);
+		this.filledellipse(2,1, 150, 170, 25, 1);
+		this.filledellipse(2,1, 350, 170, 25, 1);
+		this.filledellipse(2,1, 250, 350, 25, 1);
 		//this.geometricobjects();
+		spacing (1);
 	}
 	private void square (int x, int y, int w, int i){
 		for (int j = x; j<=(w+x); j++){
@@ -67,10 +70,25 @@ public class BIO extends Grid2D {
 		}		
 		
 	}
+	
+	public void spacing (double val){
+		super.setSpacing (val,val);
+		double origin = -(this.getWidth()-1)*(val/2);
+		super.setOrigin(origin,origin);
+	}
+		
+	
 	public static void main (String[] args){
 		new ImageJ();
 		BIO b = new BIO(512,512);
-		b.show();
+		b.show("phantom1");
+		//NumericPointwiseOperators.addBy(b, b);
+		NumericGrid added = NumericPointwiseOperators.addedBy(b, b);
+		added.show("addiert");
+		double point [] = b.indexToPhysical (0,0);
+		System.out.println (point[1]);
+		
+				
 	}
 }
  
